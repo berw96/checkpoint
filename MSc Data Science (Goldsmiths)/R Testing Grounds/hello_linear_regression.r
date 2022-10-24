c95c459d066e0d53438fe34a0da85204e7dc771f
Elliot Walker <berw96@gmail.com>
2022-10-24 14:48:16 +0100

    241022-14:47
    Completed hello_linear_regression.r

--- MSc Data Science (Goldsmiths)/R Testing Grounds/hello_linear_regression.r
+++ MSc Data Science (Goldsmiths)/R Testing Grounds/hello_linear_regression.r
@@ -1,3 +1,17 @@
 library(MASS)
 library(ISLR)
 
+#fix(Boston)
+names(Boston)
+
+lm.fit = lm(medv~lstat, data = Boston)
+
+
+confint(lm.fit)
+
+plot(lstat, medv)
+abline(lm.fit)
+
+predict(lm.fit, 
+        data.frame(lstat = c(5, 10, 15)), 
+        interval =  "prediction")
