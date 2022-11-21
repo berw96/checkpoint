23573d41791408d18444a1a3378d9cc826704ca3
erw38uob <E.walker5@uni.brighton.ac.uk>
2022-11-21 02:10:11 +0000

    211122-02:09
    Added a Jupyter Notebook for second EDA assignment, and an empty Word document which will be used to contain useful equations used throughout statistics.

---
+++ MSc Data Science (Goldsmiths)/Python Testing Grounds/Visual Studio Code/hello_NLP.py
@@ -0,0 +1,19 @@
+import nltk
+
+# list of resources for NLP
+resources = [
+    "names",
+    "stopwords",
+    "state_union",
+    "twitter_samples",
+    "movie_reviews",
+    "averaged_perceptron_tagger",
+    "vader_lexicon",
+    "punkt",
+    "shakespeare"
+]
+
+# NLTK's download manager
+nltk.download(resources)
+
+    
