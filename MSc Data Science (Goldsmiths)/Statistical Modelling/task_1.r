259550c1303599c33d8aa657a90c75d694beba7d
erw38uob <E.walker5@uni.brighton.ac.uk>
2022-12-11 19:25:49 +0000

    111222-19:24
    Task 1: Added code to search for duplicates in data.

--- MSc Data Science (Goldsmiths)/Statistical Modelling/task_1.r
+++ MSc Data Science (Goldsmiths)/Statistical Modelling/task_1.r
@@ -7,12 +7,71 @@
 # (Set as working directory first!)
 data = read.csv("datasets/Auto.csv")
 
+scan_for_copies <- function(cops){
+  # scan the dataset for copies based on cylinders, model and year.
+  # Categorical copies skew statistical significance.
+  
+  # Search through all observations in dataset.
+  for(i in 1:length(data$name)){
+    # Starting from the current observation 'i'
+    # scan through all remaining observations 'j'.
+    for(j in i:length(data$name)){
+      if(
+        i != j &&
+        data$cylinders[i] == data$cylinders[j] &&
+        data$name[i] == data$name[j] &&
+        data$year[i] == data$year[j]
+      ){
+        print("Copy found")
+        # If a copy of 'i' is found, push both to the dataframe
+        cops = rbind(cops, data[i,], data[j,])
+      }
+    }
+  }
+  # Return a dataframe of duplicates as a result.
+  return(cops)
+}
+
+##############################################################################
+# PREPROCESSING
+
+# Create a dataframe to store duplicate car models.
+copies = data.frame()
+copies = scan_for_copies(copies)
+
+# Remove duplicates from original based on index.
+data = data[!(row.names(data) %in% row.names(copies)),]
+
+# Interpret horsepower as an integer instead of a char.
+copies[,4] = as.integer(copies[,4])
+
+# Calculate the arithmetic mean of the duplicates'.
+# First initialize an empty dataframe containing all fields.
+mean_of_copies = data.frame(
+  mpg = c(0), 
+  cylinders = c(0), 
+  displacement = c(0), 
+  horsepower = c(0),
+  weight = c(0),
+  acceleration = c(0),
+  year = c(copies$year[1]),
+  origin = c(copies$origin[1]),
+  name = (copies$name[1])
+  )
+
+for(i in 1:6){
+  mean_of_copies[i] = mean(copies[,i])
+}
+
+# Append mean of duplicates to original dataframe.
+data = rbind(data, mean_of_copies)
+
 # Extract only the numeric data from our dataset.
 # We exclude the 'name' column.
 numeric_data = data[,-9]
 
 # Convert numeric data represented in non-numeric form.
-# Engine 'displacement' is represented as 'chr' strings.
+# Engine 'horsepower' is represented as 'chr' strings.
 numeric_data[,4] = as.integer(numeric_data[,4])
 
 # Calculate mean of horsepower excluding NA values.
@@ -28,11 +87,34 @@ for(i in 1:length(numeric_data[,4])){
 # Print summary of numeric data.
 summary(numeric_data)
 
+##############################################################################
+# VISUALIZATION 1
+
+#install.packages('corrplot')
+library(corrplot)
+#install.packages('ggplot2')
+library(ggplot2)
+
 # Produce scatterplot matrix for all variables in Auto dataset.
 pairs(numeric_data)
 
 # Create correlation matrix of all numeric fields in dataset.
 cor(numeric_data)
+
+# Create a correlation plot for all variables.
+corrplot(cor(numeric_data))
+
+# Univariate, individual scatterplots for mpg.
+y = numeric_data$mpg
+
+plot(x = numeric_data$cylinders, y)
+plot(x = numeric_data$displacement, y)
+plot(x = numeric_data$horsepower, y)
+plot(x = numeric_data$weight, y)
+plot(x = numeric_data$year, y)
+
+##############################################################################
+# ANALYSIS 1
 
 # Partition numeric data into training and test sets for model.
 # install.packages("caTools")
@@ -47,45 +129,67 @@ train = unlist(subset(numeric_data, split == TRUE))
 # Test set containing 20% of dataset's entries.
 test = unlist(subset(numeric_data, split == FALSE))
 
-# Create a multivariate linear regression model.
-linear_model = lm(
+# Create a multivariate linear regression model using
+# all predictor variables, denoted with '.'
+linear_model1 = lm(
   formula = mpg ~ .,
   data = numeric_data,
   subset = train
   )
 
-summary(linear_model)
+summary(linear_model1)
+
+# Now that we have trained our model using the training data, we can now
+# utilize it to make predictions for the response/output variable 'mpg'.
+prediction1 = predict(
+  linear_model1,
+  newdata = as.list(test)
+)
+
+##############################################################################
+# VISUALIZATION 2
+
+# Plot model's prediction for mpg against actual value.
+y = prediction1
+
+plot(x = numeric_data$cylinders, y, xlab = "Cylinders", ylab = "Prediction A (MPG)")
+plot(x = numeric_data$displacement, y, xlab = "Displacement", ylab = "Prediction A (MPG)")
+plot(x = numeric_data$horsepower, y, xlab = "Horsepower", ylab = "Prediction A (MPG)")
+plot(x = numeric_data$weight, y, xlab = "Weight", ylab = "Prediction A (MPG)")
+plot(x = numeric_data$year, y, xlab = "Year", ylab = "Prediction A (MPG)")
+
+##############################################################################
+# ANALYSIS 2
 
 # Second iteration of linear model after backwards elimination is applied to
 # exclude statistically insignificant variables to enhance model accuracy.
 # Weight and year are consistently significant and yield a high
 # adjusted R-squared statistic, implying high impact on response variable.
-linear_model = lm(
+linear_model2 = lm(
   formula = mpg ~ weight + year,
   data = numeric_data,
   subset = train
 )
 
-summary(linear_model)
+summary(linear_model2)
 
 # Now that we have trained our model using the training data, we can now
 # utilize it to make predictions for the response/output variable 'mpg'.
-prediction = predict(
-  object = linear_model, 
+prediction2 = predict(
+  object = linear_model2, 
   newdata = as.list(test)
   )
 
-plot(prediction, mpg)
+##############################################################################
+# VISUALIZATION 3
+
+plot(x = prediction2, y = numeric_data$mpg)
 
 abline(a = 0, b = 1)
 
-boxplot(
-  mpg, 
-  cylinders,
-  horsepower,
-  weight,
-  acceleration,
-  year,
-  origin
-  )
+
+
+
+
+
 
